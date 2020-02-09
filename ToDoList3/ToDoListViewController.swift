e9498cbc91465ea5a484a6d264b06dbd0dea007a
Benjamin Tsai <benjamintsa@gmail.com>
2020-02-09 14:15:17 -0500

    Editing/Moving TableViews (5.5)

--- ToDoList3/ToDoListViewController.swift
+++ ToDoList3/ToDoListViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ToDoListViewController: UIViewController {
 
     @IBOutlet weak var toDoTableView: UITableView!
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
+    
     var toDoArray = ["Graduate", "Get a Job", "Get Married", "Have Kids"]
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +43,19 @@ class ToDoListViewController: UIViewController {
             toDoTableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
         }
     }
+    @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
+        if toDoTableView.isEditing{
+            toDoTableView.setEditing(false, animated: true)
+            sender.title = "Edit"
+            addBarButton.isEnabled = true
+            
+        }else{
+            toDoTableView.setEditing(true, animated: true)
+            sender.title = "Done"
+            addBarButton.isEnabled = false
+        }
+    }
+    
 }
 extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -54,7 +69,17 @@ extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource{
         cell.textLabel?.text = toDoArray[indexPath.row]
         return cell
     }
-    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            toDoArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let itemToMove = toDoArray[sourceIndexPath.row]
+        toDoArray.remove(at: sourceIndexPath.row)
+        toDoArray.insert(itemToMove, at: destinationIndexPath.row)
+    }
     
 }
 
